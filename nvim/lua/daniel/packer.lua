277e54729e0001db293447a48613cdc5695b461a
Daniel Carroll <danalexanderc@gmail.com>
2025-05-22 21:09:23 -0400

    tmux simplified

--- nvim/lua/daniel/packer.lua
+++ nvim/lua/daniel/packer.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
   use('williamboman/mason-lspconfig.nvim')
   use('neovim/nvim-lspconfig')
   use('L3MON4D3/LuaSnip')
+  use('MeanderingProgrammer/render-markdown.nvim')
 
 
 end)
